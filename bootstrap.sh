f594393f384d3f75984334a5087befc0ace349d9
Fredrik Dahlstrand <32812964+fdahlstrand@users.noreply.github.com>
2023-04-14 19:28:27 +0200

    Bootstrap git configuration and clone repo

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,3 +3,12 @@
 if ! [ -x "$(command -v nix-env)" ]; then
     sh <(curl -L https://nixos.org/nix/install) --no-daemon
 fi
+
+if ! [ -x "$(command -v git)" ]; then
+    nix-env -iA git
+fi
+
+if ! [ -d "./.git" ]; then
+    git clone git@github.com:fdahlstrand/.dotfiles.git tmp
+    mv tmp/* tmp/.* .
+fi
