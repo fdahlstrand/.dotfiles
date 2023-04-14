e20b6f08e70811ac8aa2112f60614393cb636ad3
Fredrik Dahlstrand <32812964+fdahlstrand@users.noreply.github.com>
2023-04-14 22:12:53 +0200

    Download and execute nix installation script
    Make it more robust to download the nix installation script. Requires
    either wget or curl.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,17 @@
-#!/bin/bash
+#!/bin/sh
 
-if ! [ -x "$(command -v nix-env)" ]; then
-    sh <(curl -L https://nixos.org/nix/install) --no-daemon
+if ! [ -x "$(command -v nix-env)" ] 
+then
+    if [ -x "$(command -v curl)" ]
+    then
+        curl -L https://nixos.org/nix/install | sh -s -- --no-daemon
+    elif [ -x "$(command -v xwget)" ]
+    then
+        wget -qO- https://nixos.org/nix/install | sh -s -- --no-daemon
+    else
+        echo "ERROR: Need curl or wget to be able to download nix"
+        exit 1
+    fi
 fi
 
 if ! [ -x "$(command -v git)" ]; then
