88e89b3c5ed4e3a7dc2fdbf2d5c7ce55ceeffe14
Fredrik Dahlstrand <32812964+fdahlstrand@users.noreply.github.com>
2023-11-05 22:23:50 +0100

    Add cljure support to nvim

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -3,4 +3,7 @@ return {
   'tpope/vim-sleuth',
   'tpope/vim-surround',
   'christoomey/vim-tmux-navigator',
+  'guns/vim-sexp',
+  'tpope/vim-sexp-mappings-for-regular-people',
+  'tpope/vim-repeat',
 }
