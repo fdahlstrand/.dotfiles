b93523c03e6aa35c86908778083c7ee09d3a819f
Fredrik Dahlstrand <32812964+fdahlstrand@users.noreply.github.com>
2023-12-28 10:08:43 +0100

    Ensure clojure treesitter support

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -6,7 +6,7 @@ return {
   build = ':TSUpdate',
   config = function()
     require('nvim-treesitter.configs').setup {
-      ensure_installed = { 'c', 'lua', 'rust' },
+      ensure_installed = { 'c', 'lua', 'rust', 'clojure' },
       auto_install = false,
 
       highlight = { enable = true },
