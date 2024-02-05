d1a2cd880b8940736de1f81dd373def80052530d
Fredrik Dahlstrand <32812964+fdahlstrand@users.noreply.github.com>
2024-02-05 19:45:42 +0100

    Conceal links in neovim orgmode

--- nvim/.config/nvim/after/ftplugin/org.lua
+++ nvim/.config/nvim/after/ftplugin/org.lua
@@ -1,4 +1,6 @@
-vim.opt_local.textwidth = 78
-vim.opt_local.wrap      = false
-vim.opt_local.spell     = true
-vim.opt_local.spelllang = "en"
+vim.opt_local.textwidth    = 78
+vim.opt_local.wrap         = false
+vim.opt_local.spell        = true
+vim.opt_local.spelllang    = "en"
+vim.opt_local.conceallevel = 2
+vim.opt_local.concealcuror = 'nc'
