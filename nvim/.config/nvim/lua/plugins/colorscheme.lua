02e7b867e6a9c0553f871046913872a5e827d5c2
Fredrik Dahlstrand <32812964+fdahlstrand@users.noreply.github.com>
2024-10-11 17:29:19 +0200

    Update colorscheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -2,10 +2,5 @@ return {
   "folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
-  config = function()
-    vim.cmd([[colorscheme tokyonight-night]])
-
-    local colors = require("tokyonight.colors")
-    vim.cmd([[highlight ColorColumn guibg=]] .. colors.default.bg_highlight)
-  end,
+  opts = {},
 }
