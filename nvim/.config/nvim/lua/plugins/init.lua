ff20019b9ec6dda7b8e05b559b7db4129e1c991c
Fredrik Dahlstrand <32812964+fdahlstrand@users.noreply.github.com>
2025-01-16 19:21:50 +0100

    Remove old neovim configuration

--- nvim/.config/nvim/lua/plugins/init.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'tpope/vim-sleuth',
-  'tpope/vim-surround',
-  'christoomey/vim-tmux-navigator',
-  'tpope/vim-repeat',
-  'dhruvasagar/vim-table-mode',
-  'mbbill/undotree',
-  'tpope/vim-commentary'
-}
