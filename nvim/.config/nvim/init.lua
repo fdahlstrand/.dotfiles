c93115f8e01eeeb89e85eaa8fd2dbf0bcd25d17f
Fredrik Dahlstrand <32812964+fdahlstrand@users.noreply.github.com>
2025-01-16 22:24:34 +0100

    Add package managage and colorscheme to neovim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -33,6 +33,9 @@ vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣', eol = '↲' }
 
 vim.opt.inccommand = 'split'
 
+vim.opt.colorcolumn = '78'
+vim.opt.cursorline = true
+
 vim.opt.scrolloff = 10
 
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
@@ -49,6 +52,31 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function()
     vim.highlight.on_yank()
   end,
+})
+
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
+end ---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup({
+  {
+    'folke/tokyonight.nvim',
+    priority = 1000,
+    opts = {
+      on_highlights = function(highlights, colors)
+        highlights.ColorColumn.bg = colors.bg_highlight
+      end,
+    },
+    init = function()
+      vim.cmd.colorscheme 'tokyonight-night'
+    end
+  }
 })
 
 -- vim: ts=2 sts=2 sw=2 et
