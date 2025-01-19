a64a47a2b3f87d3cb6f2d8915ca0b025e0f219fa
Fredrik Dahlstrand <32812964+fdahlstrand@users.noreply.github.com>
2025-01-19 17:11:53 +0100

    Add completion and snippet support to neovim

---
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -0,0 +1,65 @@
+return {
+  'hrsh7th/nvim-cmp',
+  event = 'InsertEnter',
+  dependencies = {
+    {
+      'L3MON4D3/LuaSnip',
+      version = 'v2.*',
+      build = 'make install_jsregexp',
+    },
+    'saadparwaiz1/cmp_luasnip',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-path',
+  },
+  config = function()
+    vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
+    vim.opt.shortmess:append 'c'
+
+    local cmp = require 'cmp'
+    local luasnip = require 'luasnip'
+
+    luasnip.config.setup {}
+
+    cmp.setup {
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      mapping = {
+        ['<C-n>'] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
+        ['<C-p>'] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
+        ['<C-y>'] = cmp.mapping(
+          cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Insert,
+            select = true
+          },
+          { 'i', 'c' }
+        ),
+        ['<C-k>'] = cmp.mapping(function()
+          if luasnip.expand_or_locally_jumpable() then
+            luasnip.expand_or_jump()
+          end
+        end, { 'i', 's' }),
+        ['<C-j>'] = cmp.mapping(function()
+          if luasnip.locally_jumpable(-1) then
+            luasnip.jump(-1)
+          end
+        end, { 'i', 's' }),
+      },
+      sources = {
+        {
+          name = 'lazydev',
+          group_index = 0,
+        },
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+        { name = 'path' },
+      },
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
+    }
+  end,
+}
--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -14,10 +14,13 @@ return {
       { 'j-hui/fidget.nvim', opts = {} },
     },
     config = function()
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+
       require'lspconfig'.lua_ls.setup {
+        capabilities = capabilities,
         cmd = { vim.fn.expand('$HOME/language-servers/lua-language-server/bin/lua-language-server') }
       }
-      
     end,
   },
 }
