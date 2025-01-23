0d4e296c7ddb62d3f525157d37a0f1393f54255b
Fredrik Dahlstrand <32812964+fdahlstrand@users.noreply.github.com>
2025-01-23 22:51:35 +0100

    Throttle parallelism for lazy.nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -65,18 +65,9 @@ end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
-  'christoomey/vim-tmux-navigator',
-  {
-    'folke/tokyonight.nvim',
-    priority = 1000,
-    opts = {
-      on_highlights = function(highlights, colors)
-        highlights.ColorColumn.bg = colors.bg_highlight
-      end,
-    },
-    init = function()
-      vim.cmd.colorscheme 'tokyonight-night'
-    end
+  spec = {
+    { import = 'plugins' },
   },
-  { import = 'plugins' }
+  concurrency = 4,
 })
+
---
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -0,0 +1,16 @@
+return {
+    'christoomey/vim-tmux-navigator',
+  {
+    'folke/tokyonight.nvim',
+    priority = 1000,
+    opts = {
+      on_highlights = function(highlights, colors)
+        highlights.ColorColumn.bg = colors.bg_highlight
+      end,
+    },
+    init = function()
+      vim.cmd.colorscheme 'tokyonight-night'
+    end
+  },
+
+}
