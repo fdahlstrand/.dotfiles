6b3beb193e1ebabd23bd93a2c814b578ce331c61
Fredrik Dahlstrand <32812964+fdahlstrand@users.noreply.github.com>
2025-11-21 22:04:50 +0100

    Cleanup old configuration, start from scratch

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
-apt update
-apt dist-upgrade
 
-apt install software-properties-common
+function get_distro() {
+	if [[ -f /etc/os-release ]]
+	then
+		source /etc/os-release
+		echo $ID
+	else
+		>&2 echo "Don't know how to determine distro."
+		exit 1
+	fi
+}
 
-apt-add-repository --yes --update ppa:ansible/ansible
-apt install ansible
+case $(get_distro) in
+	ubuntu)
+		echo "Installing..."
+		;;
+	*)
+		echo "Distro not supported"
+esac	
--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++
@@ -1,4 +0,0 @@
-vim.opt_local.textwidth = 78
-vim.opt_local.wrap      = false
-vim.opt_local.spell     = true
-vim.opt_local.spelllang = "en"
