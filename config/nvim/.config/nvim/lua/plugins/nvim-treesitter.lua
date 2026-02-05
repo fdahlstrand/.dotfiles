ec72931a93d5b8767343593bc6fa442c803aed12
Fredrik Dahlstrand <32812964+fdahlstrand@users.noreply.github.com>
2026-02-05 17:42:26 +0100

    Support for Fedora distro

--- config/nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ config/nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,15 +1,16 @@
-  return {
-    'nvim-treesitter/nvim-treesitter',
-    build = 'TSUpdate',
-    main = 'nvim-treesitter.configs',
-    opts = {
-      ensure_installed = { 'bash', 'c', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
-      auto_install = true,
-      highlight = {
-        enable = true
-      },
-      indent = {
-        enable = true
-      }
+return {
+  'nvim-treesitter/nvim-treesitter',
+  branch = 'master',
+  build = 'TSUpdate',
+  main = 'nvim-treesitter.configs',
+  opts = {
+    ensure_installed = { 'bash', 'c', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
+    auto_install = true,
+    highlight = {
+      enable = true
+    },
+    indent = {
+      enable = true
     }
   }
+}
